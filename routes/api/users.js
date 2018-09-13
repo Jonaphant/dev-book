26ce8a273ed829325160f569c9b737b6d5ff559f
JonTheHero <jonathansou7@gmail.com>
2018-09-12 19:41:52 -0700

    Fixes minor syntax error on Users.js and finished implementing bcrypt for register endpoint.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -30,6 +31,17 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
